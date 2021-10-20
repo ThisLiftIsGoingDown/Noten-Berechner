5ee90bd2564a4f01940d5d85ce3216cb7dd922d8
David Bartsch <david.bartsch@bluewin.ch>
2021-10-20 21:33:15 +0200

    Version 2.0 Finished, changed to wxpython

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import wx
+from wx.core import BORDER
 import wx.grid
 from markCalc import MarkCalculator
 
@@ -19,21 +20,27 @@ class Notenskalierer (wx.Frame):
         vbox = wx.BoxSizer(wx.VERTICAL)
         gridBox = wx.FlexGridSizer(5, 2, 10, 10)
 
-        self.titleText = wx.StaticText(self.panel, label = "Notenskalen Berechnen")
+        self.titleFont = wx.Font(14, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
+        self.titleFont.Bold()
 
+        self.basicFont = wx.Font(12, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
+        self.basicFont.Bold()
+
+        self.titleText = wx.StaticText(self.panel, label = "Notenskalen Berechnen")
+        self.titleText.SetFont(self.titleFont)
         self.basicText = wx.TextCtrl(self.panel, -1, "", size=(175, -1))
         someTxt = wx.StaticText(self.panel, -1, "Maxmalpunktzahl:")
+        someTxt.SetFont(self.basicFont)
 
-        self.percentageText = wx.StaticText(self.panel, -1, "Test prozent:")
+        self.percentageText = wx.StaticText(self.panel, -1, "Prozent für Note 4:")
+        self.percentageText.SetFont(self.basicFont)
         self.percentage = wx.TextCtrl(self.panel, -1, "", size=(175, -1))
 
 
         okButton = wx.Button(self.panel, -1, "OK")
         okButton.Bind(wx.EVT_BUTTON, self.OnOk)
 
-        
-
-        
+    
 
         gridBox.AddMany([ (someTxt, 1, wx.EXPAND), (self.basicText, 1, wx.EXPAND)])
         gridBox.AddMany([ (self.percentageText, 1, wx.EXPAND), (self.percentage, 1, wx.EXPAND)])
@@ -49,17 +56,22 @@ class Notenskalierer (wx.Frame):
         menu = wx.MenuBar()
         fileMenu = wx.Menu()
         fileSettings = fileMenu.Append(wx.ID_ANY, '&Einstellungen', 'Einstellungen')
-        fileQuit = fileMenu.Append(wx.ID_EXIT, '&Beenden', 'Beenden')        
+        fileAbout = fileMenu.Append(wx.ID_ABOUT, '&Info', 'Info')
+        fileQuit = fileMenu.Append(wx.ID_EXIT, '&Beenden', 'Beenden')   
         menu.Append(fileMenu, '&Datei')
         self.SetMenuBar(menu)
         #Menubar Bindings
         self.Bind(wx.EVT_MENU, self.OnQuit, fileQuit)
         self.Bind(wx.EVT_MENU, self.OnSettings, fileSettings)
+        self.Bind(wx.EVT_MENU, self.OnAbout, fileAbout)
         self.SetTitle("Notenskalierer")
         
         self.Centre()
         self.Show(True)
-
+    #Action functions
+    def OnAbout(self, e):
+        Info(self, "Info")
+   
     def OnQuit(self, e):
         self.Close()
     
@@ -73,7 +85,7 @@ class Notenskalierer (wx.Frame):
             self.percentage.Disable()
         else:
             self.percentage.Enable()
-
+    #Calculate and display the results
     def OnOk(self, e):
         self.marks = list()
         self.settings=wx.Config("Notenskalierer")
@@ -96,16 +108,34 @@ class Notenskalierer (wx.Frame):
                 self.numberError = wx.MessageDialog(self, "Bitte geben Sie eine Zahl zwischen 0 und 100 ein!", "Fehler", wx.OK | wx.ICON_ERROR)
                 self.numberError.ShowModal()
                 return
+            if tmp2 > 70:
+                self.numberWarning = wx.MessageDialog(self, "Das ist eine sehr hohe Prozentzahl!\nBei hohen Prozentsätzen kann es vorkommen,\ndass höhere noten die Maximalpunktzahl überschreiten!\nMöchten sie fortfahren?", "Warnung", wx.YES_NO | wx.ICON_WARNING)
+                if self.numberWarning.ShowModal() == wx.ID_NO:
+                    return
             print(MarkCalculator.calculateMarkScalePercent(tmp, tmp2))
             self.marks.append(MarkCalculator.calculateMarkScalePercent(tmp, tmp2))
-            ResultDialogue(self, 'Ergebnisse', marks=self.marks)
+            
             
         else:
-            percentages = [self.settings.ReadFloat(f'{zug}Percent') for zug in ['p', 'e', 'a']]
+            percentages = [self.settings.ReadFloat(f'{zug}Prozent') for zug in ['p', 'e', 'a']]
             for percent in percentages:
                 self.marks.append(MarkCalculator.calculateMarkScalePercent(tmp, percent))
                 print(MarkCalculator.calculateMarkScalePercent(tmp, percent))
         ResultDialogue(self, 'Ergebnisse', marks=self.marks)
+
+class Info(wx.Dialog):
+    def __init__(self, parent, title):
+        wx.Dialog.__init__(self, parent, title=title, size=(350, 200))
+        self.SetBackgroundColour('#696969')
+        self.InitUI()
+        self.Centre()
+        self.Show(True)
+    
+    def InitUI(self):
+        aBox = wx.BoxSizer(wx.VERTICAL)
+        infoText = wx.StaticText(self, label = "Dieses Programm dient der Berechnung von Notenskalen.\nEs ist möglich gleich drei Skalen für die drei Lehrplan 21 Leistungszüge\nA, E und P zu erstellen. Die Mindestprozente können\nin den Einstellungen verändert werden. \nDie möglichkeit nur eine Skala zu berechnen besteht natürlich auch.\n\n\nVersion 2.0\n©2021 David Bartsch\ngithub.com/ThisLiftIsGoingDown/\n\nVielen Dank an:\nTommaso Peduzzi\ngithub.com/tommasopeduzzi ")
+        aBox.Add(infoText, 0, wx.ALIGN_CENTER)
+        self.SetSizerAndFit(aBox)
 class ResultDialogue(wx.Dialog):
      def __init__(self, parent, title, marks):
             super(ResultDialogue, self).__init__(parent, title = title, size = (350, 200))
@@ -170,12 +200,18 @@ class SettingsDialogue(wx.Dialog):
 
             self.filler = wx.StaticText(self.panel, -1, "")
 
-            self.pzug = wx.StaticText(self.panel, -1, "P-Zug Prozent")
-            self.azug = wx.StaticText(self.panel, -1, "A-Zug Prozent")
-            self.ezug = wx.StaticText(self.panel, -1, "E-Zug Prozent")
+            self.pzug = wx.StaticText(self.panel, -1, "P-Zug Prozent Note 4")
+            self.ezug = wx.StaticText(self.panel, -1, "E-Zug Prozent Note 4")
+            self.azug = wx.StaticText(self.panel, -1, "A-Zug Prozent Note 4")
+            
             self.pProzent = wx.TextCtrl(self.panel, -1, f"{self.settings.ReadFloat('pProzent')}", size=(175, -1))
             self.eProzent = wx.TextCtrl(self.panel, -1, f"{self.settings.ReadFloat('eProzent')}", size=(175, -1))
             self.aProzent = wx.TextCtrl(self.panel, -1, f"{self.settings.ReadFloat('aProzent')}", size=(175, -1))
+
+            if not self.settings.ReadBool('multipleScales'):
+                self.pProzent.Disable()
+                self.eProzent.Disable()
+                self.aProzent.Disable()
 
             self.okButton = wx.Button(self.panel, -1, "OK")
             self.cancelButton = wx.Button(self.panel, -1, "Abbrechen")
@@ -183,8 +219,8 @@ class SettingsDialogue(wx.Dialog):
             self.okButton.Bind(wx.EVT_BUTTON, self.OnOk)
             self.cancelButton.Bind(wx.EVT_BUTTON, self.OnCancel)
 
-            buttonGrid.AddMany([(self.okButton, 1, wx.EXPAND), (self.cancelButton, 1, wx.EXPAND)])
-            zugGrid.AddMany([ (self.pzug, 1, wx.EXPAND), (self.pProzent, 1, wx.EXPAND), (self.azug, 1, wx.EXPAND), (self.aProzent, 1, wx.EXPAND), (self.ezug, 1, wx.EXPAND), (self.eProzent, 1, wx.EXPAND)])
+            buttonGrid.AddMany([(self.cancelButton, 1, wx.EXPAND), (self.okButton, 1, wx.EXPAND)])
+            zugGrid.AddMany([ (self.pzug, 1, wx.EXPAND), (self.pProzent, 1, wx.EXPAND), (self.ezug, 1, wx.EXPAND), (self.eProzent, 1, wx.EXPAND), (self.azug, 1, wx.EXPAND), (self.aProzent, 1, wx.EXPAND)])
             zugGrid.AddMany([(self.filler, 1, wx.EXPAND),(buttonGrid, 1, wx.EXPAND) ])
             boxSizerSettings.Add(self.multipleScale, 0, wx.ALIGN_CENTER)
             boxSizerSettings.Add(zugGrid, wx.ID_ANY, wx.EXPAND | wx.ALL, 10)
@@ -210,6 +246,14 @@ class SettingsDialogue(wx.Dialog):
             self.parent.UpdateSettings()
 
         def OnOk(self, e):
+            if self.pProzent.GetValue() == '' or self.eProzent.GetValue() == '' or self.aProzent.GetValue() == '':
+                wx.MessageBox('Bitte alle Werte eingeben!', 'Fehler', wx.OK | wx.ICON_ERROR)
+            if float(self.pProzent.GetValue()) > 100 or float(self.eProzent.GetValue()) > 100 or float(self.aProzent.GetValue()) > 100:
+                wx.MessageBox('Bitte nur Zahlen unter 100%', 'Fehler', wx.OK | wx.ICON_ERROR)
+            if float(self.pProzent.GetValue()) > 70 or float(self.eProzent.GetValue()) > 70 or float(self.aProzent.GetValue()) > 70:
+                self.numberWarning = wx.MessageDialog(self, "Das ist eine sehr hohe Prozentzahl!\nBei hohen Prozentsätzen kann es vorkommen,\ndass höhere noten die Maximalpunktzahl überschreiten!\nMöchten sie fortfahren?", "Warnung", wx.YES_NO | wx.ICON_WARNING)
+                if self.numberWarning.ShowModal() == wx.ID_NO:
+                    return
             self.Save()
             self.Close() 
 
